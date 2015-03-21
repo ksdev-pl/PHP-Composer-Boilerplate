199342f0d8b798fbec75c82ed2a2d6c923ec2ae0
ksdev <ks.cloudbox@gmail.com>
2015-03-21 17:02:36 +0100

    Added Whoops exception handler and Dotenv

--- public/index.php
+++ public/index.php
@@ -5,13 +5,38 @@
  */
 
 error_reporting(E_ALL);
-ini_set("display_errors", 1);
 
 date_default_timezone_set('Europe/Warsaw');
 
 define('ROOT', dirname(__DIR__));
 
 require_once ROOT . '/vendor/autoload.php';
+
+Dotenv::load(ROOT);
+
+$logger = App\Logger::getInstance();
+$whoops = new \Whoops\Run;
+if (getenv('APP_DEBUG') === 'true') {
+    ini_set('display_errors', 'On');
+
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+    $whoops->pushHandler(function ($exception) use ($logger) {
+        $logger->addError((string) $exception);
+    });
+}
+else {
+    ini_set('display_errors', 'Off');
+
+    $whoops->pushHandler(function ($exception) use ($logger) {
+        $logger->addError((string) $exception);
+        http_response_code(500);
+        exit('A website error has occurred.
+            The website administrator has been notified of the issue.
+            Sorry for the temporary inconvenience.');
+    });
+}
+
+$whoops->register();
 
 
 /************************************************
@@ -57,6 +82,8 @@ $klein->respond('GET', '/', function () {
 ──██─────▐█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▌
 ──██────▐█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▌
     </pre>';
+
+    $db = new App\DB('localhost', 'root', 'root', 'app');
 
 });
 
